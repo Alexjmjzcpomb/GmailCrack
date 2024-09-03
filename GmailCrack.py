db3ef390f8214e384559fbe1b90612350b2a9cad
Alexjmjzcpomb <cheatcheat348@gmail.com>
2024-09-03 11:34:09 +0700

    Update GmailCrack.py

--- GmailCrack.py
+++ GmailCrack.py
@@ -1,30 +1,5 @@
-# Program: Gmail Dictionary Attack v2
 
-print"Join Ethical Hacker's Community"
-#Information
-print "Author: Kinghacker0"
-print "YouTube - www.YouTube.com/Hacker's King "
-print "Website - www.hackersking.in"
 
-print"      *                                            *   "
-print"     *                                              *    "
-print"    **                                              **   "
-print"   *   **                                        **   *    "
-print"   **   **  *                               *   **    **   "
-print"   ***    * **    Instagram-@kinghacker0   **  *    ***  "
-print"    ****    ******************************* ***   ****   "
-print"       *******    *****        *******    **********  "
-print"  ***********           *****             ************     "
-print"    **********    *** * **   ** ****    **********     "
-print"         ********** ** **     ** ****************    "
-print"   *************** ** **  ***  **  **********************  "
-print"        ******   ***************************   ******     "
-print"                *************************    "
-print"               **************************  "
-print"               **** ** ** **** ** ** **  "
-print"                ***  *  *  **  *  * ***   "
-print"                 **                 **    "
-print"                   *                *     "
 
 print" Disclaimer- This tool is only for educational purpose"
 import smtplib
